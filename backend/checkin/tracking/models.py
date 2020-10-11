1d32b6a0a458f21fe8c375e75d349ae3f1ba2556
jelko arnds <moin@jelkoarnds.com>
2020-10-12 00:38:40 +0200

    backend: tel up to 20 chars

--- backend/checkin/tracking/models.py
+++ backend/checkin/tracking/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     last_name = models.CharField(_("Nachname"), max_length=255)
     phone_regex = RegexValidator(regex=r'^\+?1?[\d ()]{9,15}$',
                                  message=_("Phone number must be entered in the format: '+(00) 999999999'. Up to 15 digits allowed."))
-    phone = models.CharField(_("Telefonnummer"), validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    phone = models.CharField(_("Telefonnummer"), validators=[phone_regex], max_length=20, blank=True) # validators should be a list
     email = models.EmailField(_("E-Mail Adresse"), blank=True)
     verified = models.BooleanField(_("Identität geprüft"),blank=True, null=True, default=False)
     updated_at = models.DateTimeField(auto_now=True)
