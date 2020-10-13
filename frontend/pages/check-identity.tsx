0c75f631b97aad439ce0a746563e17156c1daa1e
Leo <130.0x82@gmail.com>
2020-10-13 13:45:56 +0200

    frontend: check in not verfied page

---
+++ frontend/pages/check-identity.tsx
@@ -0,0 +1,11 @@
+
+import * as React from 'react';
+
+interface NotVerifiedPageProps {
+}
+
+const NotVerifiedPage: React.FunctionComponent<NotVerifiedPageProps> = (props) => {
+  return null;
+};
+
+export default NotVerifiedPage;
