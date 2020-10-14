8fc2a8d1f0adc32f135077c139106d85cc530542
Leo <130.0x82@gmail.com>
2020-10-14 03:20:02 +0200

    frontend: pwa test

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,17 +4,28 @@ import AppWrapper from "../components/common/AppWrapper";
 import { AppStateProvider } from "../components/common/AppStateProvider";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { LocaleProvider, getInitialLocale } from "../localization";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
-        <AppStateProvider>
-            <AppWrapper>
-                <LocaleProvider locale={pageProps.locale || getInitialLocale()}>
-                    <Component {...pageProps} />
-                </LocaleProvider>
-            </AppWrapper>
-        </AppStateProvider>
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+                />
+            </Head>
+            <AppStateProvider>
+                <AppWrapper>
+                    <LocaleProvider
+                        locale={pageProps.locale || getInitialLocale()}
+                    >
+                        <Component {...pageProps} />
+                    </LocaleProvider>
+                </AppWrapper>
+            </AppStateProvider>
+        </>
     );
-}
+};
 
 export default MyApp;
---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,86 @@
+import Document, {
+    DocumentContext,
+    Html,
+    Head,
+    Main,
+    NextScript,
+} from "next/document";
+import { config } from "process";
+import theme from "../styles/theme";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="manifest" href="/manifest.json" />
+
+                    <meta name="mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="application-name" content="Checkin" />
+                    <meta name="apple-mobile-web-app-title" content="Checkin" />
+                    <meta name="theme-color" content="#D81830" />
+
+                    <meta
+                        name="msapplication-navbutton-color"
+                        content="#D81830"
+                    />
+                    <meta
+                        name="apple-mobile-web-app-status-bar-style"
+                        content="black-translucent"
+                    />
+                    <meta name="msapplication-starturl" content="/" />
+
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="512x512"
+                        href="icon-512x512"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        type="image/png"
+                        sizes="512x512"
+                        href="icon-512x512"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="icon-192x192"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="icon-192x192"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="144x144"
+                        href="icon-144x144"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        type="image/png"
+                        sizes="144x144"
+                        href="icon-144x144"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
