705d2350fcaf9c283fe36b417aa554825c944a46
jelko <moin@jelkoarnds.com>
2020-10-16 16:37:28 +0200

    Update help.tsx

--- frontend/pages/help.tsx
+++ frontend/pages/help.tsx
@@ -6,7 +6,22 @@ interface HelpPageProps {}
 
 const HelpPage: React.FunctionComponent<HelpPageProps> = (props) => {
     const { t } = useTranslation();
-    return <Title>{t("Hilfe")}</Title>;
+    return (
+        <>
+            <Title>{t("Hilfe")}</Title>
+            <Notice>Unterstzung Checkin-Anwendung</Notice>
+            <Text paragraph>
+                Bitte melde Dich bei Fragen zur Anwendung und zur Protokollierung und Raumbeschilderungen, sowie Problemhinweisen bitte bei:<br/>
+                <a href="mailto:checkin@hfk-bremen.de">checkin@hfk-bremen.de</a><br/>
+                Die Anwendung und das neue Zugangsverfahren befinden sich momentan in einem Test.<br/><br/>
+                In der ersten Woche unterstützt der AStA freundlicherweise alle Nutzer*innen bei Fragen und Problemen vor dem Speicher am Chaos Office und unter <a href="tel:+4942195951060">0421 9595-1060</a>.
+            </Text>
+            <Notice>Weiterführende Informationen</Notice>
+            <Notice>
+                Jeweils aktuelle Informationen zu den Corona-Maßnahmen und Hygieneregeln findest du in deinem HfK-Postfach und unter <a href="https://www.hfk-bremen.de/corona-info" target="_blank">hfk-bremen.de/corona-info</a>. Bitte bleibe Informiert!
+            </Notice>
+        </>
+    );
 };
 
 export default HelpPage;
