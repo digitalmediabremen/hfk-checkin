e0c8de34b6b924e96934db79bf163eb544ec9303
Leo <130.0x82@gmail.com>
2020-10-16 16:46:13 +0200

    frontend: fix layout bugs and missign imports

--- frontend/pages/help.tsx
+++ frontend/pages/help.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import Title from "../components/common/Title";
 import { useTranslation } from "../localization";
+import Notice from "../components/common/Notice";
+import Text from "../components/common/Text";
+import Subtitle from "../components/common/Subtitle";
 
 interface HelpPageProps {}
 
@@ -8,17 +11,39 @@ const HelpPage: React.FunctionComponent<HelpPageProps> = (props) => {
     const { t } = useTranslation();
     return (
         <>
+            <style jsx>
+                {`
+                    a {
+                        text-decoration: underline;
+                    }
+                `}
+            </style>
             <Title>{t("Hilfe")}</Title>
-            <Notice>Unterstzung Checkin-Anwendung</Notice>
+            <Subtitle>Unterstützung Checkin-Anwendung</Subtitle>
             <Text paragraph>
-                Bitte melde Dich bei Fragen zur Anwendung und zur Protokollierung und Raumbeschilderungen, sowie Problemhinweisen bitte bei:<br/>
-                <a href="mailto:checkin@hfk-bremen.de">checkin@hfk-bremen.de</a><br/>
-                Die Anwendung und das neue Zugangsverfahren befinden sich momentan in einem Test.<br/><br/>
-                In der ersten Woche unterstützt der AStA freundlicherweise alle Nutzer*innen bei Fragen und Problemen vor dem Speicher am Chaos Office und unter <a href="tel:+4942195951060">0421 9595-1060</a>.
+                Bitte melde Dich bei Fragen zur Anwendung und zur
+                Protokollierung und Raumbeschilderungen, sowie Problemhinweisen
+                bitte bei:
+                <br />
+                <a href="mailto:checkin@hfk-bremen.de">checkin@hfk-bremen.de</a>
+                <br />
+                Die Anwendung und das neue Zugangsverfahren befinden sich
+                momentan in einem Test.
+                <br />
+                <br />
+                In der ersten Woche unterstützt der AStA freundlicherweise alle
+                Nutzer*innen bei Fragen und Problemen vor dem Speicher am Chaos
+                Office und unter <a href="tel:+4942195951060">0421 9595-1060</a>
+                .
             </Text>
-            <Notice>Weiterführende Informationen</Notice>
+            <Subtitle>Weiterführende Informationen</Subtitle>
             <Text paragraph>
-                Jeweils aktuelle Informationen zu den Corona-Maßnahmen und Hygieneregeln findest du in deinem HfK-Postfach und unter <a href="https://www.hfk-bremen.de/corona-info" target="_blank">hfk-bremen.de/corona-info</a>. Bitte bleibe Informiert!
+                Jeweils aktuelle Informationen zu den Corona-Maßnahmen und
+                Hygieneregeln findest du in deinem HfK-Postfach und unter{" "}
+                <a href="https://www.hfk-bremen.de/corona-info" target="_blank">
+                    hfk-bremen.de/corona-info
+                </a>
+                . Bitte bleibe Informiert!
             </Text>
         </>
     );
--- frontend/pages/privacy.tsx
+++ frontend/pages/privacy.tsx
@@ -11,10 +11,18 @@ const PrivacyPage: React.FunctionComponent<PrivacyPageProps> = (props) => {
     const { t } = useTranslation();
     return (
         <>
+            <style jsx>
+                {`
+                    a {
+                        text-decoration: underline;
+                    }
+                `}
+            </style>
             <Title>{t("Datenschutz")}</Title>
             <Notice>Betreiberin</Notice>
             <Subtitle>
-                Hochschule für Künste Bremen<br />
+                Hochschule für Künste Bremen
+                <br />
                 Am Speicher XI 8<br />
                 D-28217 Bremen
             </Subtitle>
@@ -23,13 +31,28 @@ const PrivacyPage: React.FunctionComponent<PrivacyPageProps> = (props) => {
             </Text>
             <Notice>Datenschutzbestimmungen</Notice>
             <Text paragraph>
-                Die Erhebung der Daten erfolgt zum Zwecke der Nachverfolgung von Infektionsketten im Zusammenhang mit dem Coronavirus SARS CoV-2. Rechtsgrundlage der Verarbeitung ist Art. 6 Abs. 1 lit. c DSGVO i.V.m. § 5 Abs. 2 Nr. 3, § 8 Achtzehnte Verordnung zum Schutz vor Neuinfektionen mit dem Coronavirus SARS-CoV-2.
+                Die Erhebung der Daten erfolgt zum Zwecke der Nachverfolgung von
+                Infektionsketten im Zusammenhang mit dem Coronavirus SARS CoV-2.
+                Rechtsgrundlage der Verarbeitung ist Art. 6 Abs. 1 lit. c DSGVO
+                i.V.m. § 5 Abs. 2 Nr. 3, § 8 Achtzehnte Verordnung zum Schutz
+                vor Neuinfektionen mit dem Coronavirus SARS-CoV-2.
             </Text>
             <Text paragraph>
-                Wir verarbeiten Ihre Daten grundsätzlich nur solange, wie sie für die Zwecke, für die sie erhoben worden sind, erforderlich sind. Daher werden Ihre Daten für die Dauer von drei Wochen aufbewahrt und nach Ablauf der Aufbewahrungsfrist vernichtet bzw. gelöscht.
+                Wir verarbeiten Ihre Daten grundsätzlich nur solange, wie sie
+                für die Zwecke, für die sie erhoben worden sind, erforderlich
+                sind. Daher werden Ihre Daten für die Dauer von drei Wochen
+                aufbewahrt und nach Ablauf der Aufbewahrungsfrist vernichtet
+                bzw. gelöscht.
             </Text>
             <Text paragraph>
-                Die <a href="https://www.hfk-bremen.de/sites/default/files/media/2020_info_datenschutz_nutzerinnen_hfk_bremen.pdf" target="_blank">vollständigen Datenschutzbestimmungen</a> dieser Anwendung finden Sie auf der Website der HfK Bremen.
+                Die{" "}
+                <a
+                    href="https://www.hfk-bremen.de/sites/default/files/media/2020_info_datenschutz_nutzerinnen_hfk_bremen.pdf"
+                    target="_blank"
+                >
+                    vollständigen Datenschutzbestimmungen
+                </a>{" "}
+                dieser Anwendung finden Sie auf der Website der HfK Bremen.
             </Text>
         </>
     );
