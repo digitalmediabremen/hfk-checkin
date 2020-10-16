f6ea26f4131671f1e270ad4d11ebfc9df73d08fc
jelko <moin@jelkoarnds.com>
2020-10-16 16:42:12 +0200

    Update privacy.tsx

--- frontend/pages/privacy.tsx
+++ frontend/pages/privacy.tsx
@@ -21,12 +21,13 @@ const PrivacyPage: React.FunctionComponent<PrivacyPageProps> = (props) => {
             <Text paragraph>
                 Vertreten durch den Rektor Prof. Roland Lambrette.
             </Text>
-            <Notice>
+            <Notice>Datenschutzbestimmungen</Notice>
+            <Text paragraph>
                 Die Erhebung der Daten erfolgt zum Zwecke der Nachverfolgung von Infektionsketten im Zusammenhang mit dem Coronavirus SARS CoV-2. Rechtsgrundlage der Verarbeitung ist Art. 6 Abs. 1 lit. c DSGVO i.V.m. § 5 Abs. 2 Nr. 3, § 8 Achtzehnte Verordnung zum Schutz vor Neuinfektionen mit dem Coronavirus SARS-CoV-2.
-            </Notice>
-            <Notice>
+            </Text>
+            <Text paragraph>
                 Wir verarbeiten Ihre Daten grundsätzlich nur solange, wie sie für die Zwecke, für die sie erhoben worden sind, erforderlich sind. Daher werden Ihre Daten für die Dauer von drei Wochen aufbewahrt und nach Ablauf der Aufbewahrungsfrist vernichtet bzw. gelöscht.
-            </Notice>
+            </Text>
             <Text paragraph>
                 Die <a href="https://www.hfk-bremen.de/sites/default/files/media/2020_info_datenschutz_nutzerinnen_hfk_bremen.pdf" target="_blank">vollständigen Datenschutzbestimmungen</a> dieser Anwendung finden Sie auf der Website der HfK Bremen.
             </Text>
