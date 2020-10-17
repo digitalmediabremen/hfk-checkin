43938c7dba1947807ec2e80c7f1e7a693e43fad6
Leo <130.0x82@gmail.com>
2020-10-17 23:01:51 +0200

    frontend: format help remove dot

--- frontend/pages/help.tsx
+++ frontend/pages/help.tsx
@@ -19,21 +19,33 @@ const HelpPage: React.FunctionComponent<HelpPageProps> = (props) => {
                 `}
             </style>
             <Title>{t("Hilfe")}</Title>
-            <Notice>Auf der Suche nach einer QR-Scanner-App? Siehe unten.</Notice>
+            <Notice>
+                Auf der Suche nach einer QR-Scanner-App? Siehe unten.
+            </Notice>
             <Subtitle>Unterstützung Checkin-Anwendung</Subtitle>
             <Text paragraph>
                 Bitte melde Dich bei Fragen zur Anwendung und zur
                 Protokollierung und Raumbeschilderungen, sowie Problemhinweisen
                 bei:
-                <br /><br />
-                <strong><a href="mailto:checkin@hfk-bremen.de">checkin@hfk-bremen.de</a></strong><br />
+                <br />
+                <br />
+                <strong>
+                    <a href="mailto:checkin@hfk-bremen.de">
+                        checkin@hfk-bremen.de
+                    </a>
+                </strong>
+                <br />
                 <br />
                 Die Anwendung und das neue Zugangsverfahren befinden sich
                 momentan in einem Testbetrieb.
                 <br />
                 <br />
-                In der ersten Woche (19.–23.10.2020) können Sie das Team der Anwendung unter <strong><a href="tel:+4942195951306">0421 9595-1306</a></strong> bei Fragen oder Problemen erreichen.
-                .
+                In der ersten Woche (19.–23.10.2020) können Sie das Team der
+                Anwendung unter{" "}
+                <strong>
+                    <a href="tel:+4942195951306">0421 9595-1306</a>
+                </strong>{" "}
+                bei Fragen oder Problemen erreichen.
             </Text>
             <Subtitle>Weiterführende Informationen</Subtitle>
             <Text paragraph>
@@ -46,10 +58,29 @@ const HelpPage: React.FunctionComponent<HelpPageProps> = (props) => {
             </Text>
             <Subtitle>QR-Scanner-App-Empfehlungen</Subtitle>
             <Text paragraph>
-                iOS und Android können seit 2017 QR-Codes mit der Standard-Kamara-App scannen. Falls du eine zusätzliche App benötigst, nutze gerne eine der folgenden <strong>kostenlosen und werbefreien QR-Scanner-Apps</strong>:<br/><br/>
-                auf Apple iOS: <a href="https://apps.apple.com/de/app/qrs-clean-fast-qr-scanner/id1042620761">QRs App</a><br/>
-                auf Android: <a href="https://play.google.com/store/apps/details?id=de.markusfisch.android.binaryeye">Binary Eye App</a><br/><br/>
-                <i>Hinweis: Es gelten die Nutzungsbedingungen der jeweiligen Anbieter und App-Stores. Trotz sorgfältiger Prüfung kann die HfK Bremen keine Verantwortung für die Apps und verlinkten Websites übernehmen.</i>
+                iOS und Android können seit 2017 QR-Codes mit der
+                Standard-Kamara-App scannen. Falls du eine zusätzliche App
+                benötigst, nutze gerne eine der folgenden{" "}
+                <strong>kostenlosen und werbefreien QR-Scanner-Apps</strong>:
+                <br />
+                <br />
+                auf Apple iOS:{" "}
+                <a href="https://apps.apple.com/de/app/qrs-clean-fast-qr-scanner/id1042620761">
+                    QRs App
+                </a>
+                <br />
+                auf Android:{" "}
+                <a href="https://play.google.com/store/apps/details?id=de.markusfisch.android.binaryeye">
+                    Binary Eye App
+                </a>
+                <br />
+                <br />
+                <i>
+                    Hinweis: Es gelten die Nutzungsbedingungen der jeweiligen
+                    Anbieter und App-Stores. Trotz sorgfältiger Prüfung kann die
+                    HfK Bremen keine Verantwortung für die Apps und verlinkten
+                    Websites übernehmen.
+                </i>
             </Text>
         </>
     );
