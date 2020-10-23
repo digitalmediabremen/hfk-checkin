a2e00a36e6fa6ce70e6d14861067db6b84c55d9a
Leo <130.0x82@gmail.com>
2020-10-23 22:21:14 +0200

    frontend: add checkin origin type

--- frontend/model/Checkin.ts
+++ frontend/model/Checkin.ts
@@ -9,3 +9,13 @@ export interface Checkin {
     profile: Profile;
     location: Location;
 }
+
+export enum CheckinOrigin {
+    QR_SCAN = "0",
+    USER_MANUAL = "1",
+    ADMIN_MANUAL = "2",
+    FOREIGN_SCAN = "3",
+    PARENT_CHECKOUT = "4",
+    IMPORT = "5",
+    UNKNOWN = "6"
+}
