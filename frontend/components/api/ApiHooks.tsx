deb7f369862e9ec9a4712db84deb68998b778335
Leo <130.0x82@gmail.com>
2020-11-04 18:33:55 +0100

    frontend: add state and refactor

--- frontend/components/api/ApiHooks.tsx
+++ frontend/components/api/ApiHooks.tsx
@@ -9,31 +9,58 @@ import {
     getProfileRequest,
     Response,
     updateProfileRequest,
+    getCheckinRequest,
 } from "./ApiService";
 import { Checkin } from "../../model/Checkin";
 
-export const useApi = <RT extends {}>(config?: {
-    onlyLocalErrorReport?: boolean;
-}): {
-    loading: boolean;
-    result?: RT;
-    success: boolean;
-    error?: string;
+type UseApiReturnType<RT extends {}> = {
     request: (request: () => Promise<Response<RT>>) => void;
-} => {
+} & (
+    | {
+          state: "initial";
+          error: undefined;
+          result: undefined;
+      }
+    | {
+          state: "loading" | "loading";
+          error: undefined;
+          result: undefined;
+      }
+    | {
+          state: "error";
+          error: string;
+          result: undefined;
+      }
+    | {
+          state: "success";
+          error: undefined;
+          result: RT;
+      }
+);
+
+export const useApi = <RT extends {}>(_config?: {
+    onlyLocalErrorReport?: boolean;
+}): UseApiReturnType<RT> => {
     const { dispatch } = useAppState();
     const [result, setResult] = useState<RT | undefined>(undefined);
     const [error, setError] = useState<string | undefined>(undefined);
     const [requestInProgress, setRequestInProgress] = useState(false);
     const loading = requestInProgress;
     const success = !error && !!result && !loading;
-    const c = config || {
+    const config = _config || {
         onlyLocalErrorReport: false,
+    };
+
+    const deriveRequestState = () => {
+        if (loading) return "loading";
+        if (error) return "error";
+        if (result) return "success";
+        return "initial";
     };
 
     const handleError = (error: string, status: number) => {
         setError(error);
-        if (!c.onlyLocalErrorReport) {
+        if (!config.onlyLocalErrorReport) {
             dispatch({
                 type: "status",
                 status: {
@@ -57,7 +84,7 @@ export const useApi = <RT extends {}>(config?: {
                 handleError(error || `Unknown Error (${status})`, status);
             } else if (!!error && status > 500) {
                 throw error;
-            } else if (!c.onlyLocalErrorReport) {
+            } else if (!config.onlyLocalErrorReport) {
                 // reset error message
                 // but only if request is reporting globally
 
@@ -71,91 +98,103 @@ export const useApi = <RT extends {}>(config?: {
     };
 
     return {
-        loading,
+        state: deriveRequestState(),
         result,
-        success,
         error,
         request: handleRequest,
-    };
+    } as UseApiReturnType<RT>;
 };
 
 export const useUpdateProfileAppState = () => {
-    const {
-        getProfile,
-        success,
-        profile,
-        loading,
-        error
-    } = useProfile();
+    const { getProfile, profile, error, loading, success } = useProfile();
     const { appState, dispatch } = useAppState();
 
-    useEffect(() => { 
-        getProfile() 
+    useEffect(() => {
+        getProfile();
     }, []);
 
     useEffect(() => {
-        success &&
+        profile &&
             dispatch({
                 type: "profile",
                 profile,
             });
-    }, [success]);
+    }, [profile]);
 
     return {
         loading,
         success,
         error,
-        profile
-    }
+        profile,
+    };
 };
 
 export const useUpdateProfile = () => {
-    const { request, ...other } = useApi<Profile>();
+    const { request, state, ...other } = useApi<Profile>();
 
     return {
         updateProfile: (profile: ProfileUpdate) =>
             request(() => updateProfileRequest(profile)),
+        success: state === "success",
+        loading: state === "loading",
         ...other,
     };
 };
 
 export const useProfile = () => {
-    const { request, result: profile, ...other } = useApi<Profile>({
+    const { request, result: profile, state, ...other } = useApi<Profile>({
         onlyLocalErrorReport: true,
     });
 
     return {
         profile,
         getProfile: () => request(() => getProfileRequest()),
+        success: state === "success",
+        loading: state === "loading",
         ...other,
     };
 };
 
 export const useLocation = () => {
-    const { request, result, ...other } = useApi<Location>();
+    const { request, result, state, ...other } = useApi<Location>();
     return {
         requestLocation: (locationCode: string) =>
             request(() => getLocationRequest(locationCode)),
         location: result,
+        success: state === "success",
+        loading: state === "loading",
         ...other,
     };
 };
 
+export const useCheckin = () => {
+    const { request, ...data } = useApi<Checkin>();
+
+    return {
+        getCheckin: (checkinId: string) =>
+            request(() => getCheckinRequest(checkinId)),
+        data,
+    };
+};
+
 export const useDoCheckin = () => {
-    const { request, result, ...other } = useApi<Checkin>();
+    const { state, request, result, ...other } = useApi<Checkin>();
     return {
         doCheckin: (locationCode: string) =>
             request(() => doCheckinRequest(locationCode)),
-        
+        success: state === "success",
+        loading: state === "loading",
         ...other,
     };
 };
 
 export const useDoCheckout = () => {
-    const { request, result, ...other } = useApi<{}>();
+    const { request, state, result, ...other } = useApi<{}>();
     return {
         doCheckout: (locationCode: string) =>
             request(() => doCheckoutRequest(locationCode)),
+        success: state === "success",
+        loading: state === "loading",
         ...other,
     };
 };
