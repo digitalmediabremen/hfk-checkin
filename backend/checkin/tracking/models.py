63d7a8cfda6089401b0f63fd905e5a4ecbf4220d
jelko arnds <moin@jelkoarnds.com>
2020-11-13 16:20:58 +0100

    backend: Shortening room number field

--- backend/checkin/tracking/models.py
+++ backend/checkin/tracking/models.py
@@ -116,7 +116,7 @@ class BookingMethod(models.Model):
 class Location(MPTTModel):
     code = models.CharField(_("Raumcode"), max_length=4, unique=True, default=pkgen)
     parent = TreeForeignKey('self', verbose_name=_('Teil von'), on_delete=models.CASCADE, null=True, blank=True, related_name='children', default=3)
-    org_number = models.CharField(_("Raumnummer"), max_length=30, blank=True, help_text=_("Speicher XI: X.XX.XXX / Dechanatstraße: K.XX"))
+    org_number = models.CharField(_("Raumnummer"), max_length=24, blank=True, help_text=_("Speicher XI: X.XX.XXX / Dechanatstraße: K.XX"))
     org_name = models.CharField(_("Raumname / Standort"), max_length=255)
     org_responsible = models.CharField(_("Raumverantwortliche(r)"), max_length=255, blank=True, null=True)
     org_size = models.DecimalField(verbose_name=_("Größe"), help_text=_("in Quadratmetern"), max_digits=8, decimal_places=2, blank=True, null=True)
