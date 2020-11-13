eb5b7f7d4301101f9b3f34005eca2089d3ee40ff
jelko arnds <moin@jelkoarnds.com>
2020-11-13 19:34:56 +0100

    frontend: extended help

--- frontend/components/help/HelpContent-de.tsx
+++ frontend/components/help/HelpContent-de.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Notice from "../common/Notice";
 import Subtitle from "../common/Subtitle";
 import Text from "../common/Text";
+import QRIcon from "../common/QRIcon";
 
 const HelpContentDe = () => {
     return (
@@ -14,35 +15,39 @@ const HelpContentDe = () => {
                 `}
             </style>
             <Notice>
-                Auf der Suche nach einer QR-Scanner-App? Siehe unten.
+                Auf der Suche nach einer QR-Scanner-App? Siehe Abschnitt 3.
             </Notice>
-            <Subtitle>Unterstützung Checkin-Anwendung</Subtitle>
+            <Subtitle>1. Wie funktioniert der Checkin and Checkout?</Subtitle>
             <Text paragraph>
-                Bitte melde Dich bei Fragen zur Anwendung und zur
-                Protokollierung und Raumbeschilderungen, sowie Problemhinweisen
-                bei:
-                <br />
-                <br />
-                <strong>
-                    <a href="mailto:checkin@hfk-bremen.de">
-                        checkin@hfk-bremen.de
-                    </a>
-                </strong>
-                <br />
-                <br />
-                Die Anwendung und das neue Zugangsverfahren befinden sich
-                momentan in einem Testbetrieb.
+                Sobald du dich registriert hast, kannst du selbstständig auf dieser Website ein Aufenthaltsprotokoll mit deinem Smartphone führen.
+                Es ist keine Installation einer App nötig. Der Dienst funktioniert auch auf PCs und Tablets.<br/><br/>
+                Scanne an jedem Standort den QR-Code <strong>oder</strong> gib den vierstelligen Raumcode ein.<br/><br/>
+                Dadurch entsteht im Laufe deines Besuchs ein Protokoll, welches im Falle einer Infektionsnachverfolgung
+                verwendet wird.<br/><br/>
+                Vergiss beim Verlassen der jeweiligen Räume und auch beim Verlassen des Speicher XI bitte nicht dich auszuchecken!
             </Text>
-            <Subtitle>Weiterführende Informationen</Subtitle>
+            <Subtitle>2. Wann muss ich mich einchecken?</Subtitle>
             <Text paragraph>
-                Jeweils aktuelle Informationen zu den Corona-Maßnahmen und
-                Hygieneregeln findest du in deinem HfK-Postfach und unter{" "}
-                <a href="https://www.hfk-bremen.de/corona-info" target="_blank">
-                    hfk-bremen.de/corona-info
-                </a>
-                . Bitte bleibe Informiert!
+                Bitte checke beim Betreten des Gebäudes, sowie in jedem Raum, ein. Falls Du mehrere Räume zu gleich nutzt,
+                ist es möglich in allen eingecheckt zu sein.
             </Text>
-            <Subtitle>QR-Scanner-App-Empfehlungen</Subtitle>
+            <Subtitle>3. Wann muss ich mich auschecken? Werde ich automatisch ausgecheckt?</Subtitle>
+            <Text paragraph>
+                Bitte checke aus, wenn Du den Raum verlässt. Kurze Unterbrechungen, z.B. Toilettenbesuche müssen nicht
+                protokolliert werden, solange es nicht zu Kontakten mit anderen Personen kommt. Automatische Checkouts
+                finden nicht statt. Checkins verfallen nach 24 Stunden.
+            </Text>
+            <Subtitle>4. Muss ich weiterhin Räume buchen?</Subtitle>
+            <Text paragraph>
+                Ja, für eine Nutzung von Büros, Ateliers und Werkstätten ist die Buchung weiterhin
+                erforderlich. Spontane und unangemeldete Nutzungen sind für ausgewählte Räume möglich. z.B. die Bibliothek, Teeküchen und die Mensa.
+            </Text>
+            <Subtitle>5. Brauche ich einen QR-Code-Scanner?</Subtitle>
+            <Text paragraph>
+                Nein. Checkins sind über 4-stellige Nummerncodes oder QR-Codes möglich. Eine Benutzung des Dienstes ist
+                ohne QR-Scanner möglich. Es wird lediglich ein Webbrowser und eine aktive Internetverbindung benötigt.
+            </Text>
+            <Subtitle>6. Wie kann ich QR-Codes scannen?</Subtitle>
             <Text paragraph>
                 iOS und Android können seit 2017 QR-Codes mit der
                 Standard-Kamara-App scannen. Falls du eine zusätzliche App
@@ -67,6 +72,52 @@ const HelpContentDe = () => {
                     HfK Bremen keine Verantwortung für die Apps und verlinkten
                     Websites übernehmen.
                 </i>
+            </Text>
+            <Subtitle>7. Wie kann ich schneller auf die Anwendung zugreifen?</Subtitle>
+            <Text paragraph>
+                Füge die Webanwendung wie eine reguläre App zum Homescreen deines Smartphones hinzu.
+                Dieses funktioniert wie ein schneller Zugriff per Lesezeichen.<br/><br/>
+
+                Unter Apple iOS:
+                <ol>
+                    <li>Öffne <i>Safari</i></li>
+                    <li>Besuche checkin.hfk-bremen.de</li>
+                    <li>Nutze die Funktion <i>Teilen</i> (&#x2191;) in der mitte der Funktionsleiste am unteren Bildschirmrand.</li>
+                    <li>Wähle <i>Zum Home-Bildschirm</i>.</li>
+                </ol>
+
+                Unter Android mit Chrome oder Firefox:
+                <ol>
+                    <li>Öffne <i>Chrome</i> oder <i>Firefox</i></li>
+                    <li>Besuche checkin.hfk-bremen.de</li>
+                    <li>Tippe am unten am Bildschirm auf die Menüschaltfläche (&#8942;).</li>
+                    <li>Wähle <i>Zum Startbildschirm hinzufügen</i>.</li>
+                </ol>
+            </Text>
+            <Subtitle>8. Fragen zu oder Probleme mit der Checkin-Anwendung?</Subtitle>
+            <Text paragraph>
+                Bitte melde dich bei Fragen zur Anwendung und zur
+                Protokollierung und Raumbeschilderungen, sowie Problemhinweisen
+                bei:
+                <br />
+                <br />
+                    <a href="mailto:checkin@hfk-bremen.de">
+                        checkin@hfk-bremen.de
+                    </a>
+                <br />
+                <br />
+                Die Anwendung und das neue Zugangsverfahren befinden sich
+                momentan in einem Testbetrieb.
+            </Text>
+            <Subtitle>9. Weiterführende Informationen zu Corona</Subtitle>
+            <Text paragraph>
+                Viele Antworten zum Studium ab dem WiSe 2020 an der HfK wurden
+                auf <a href="https://faq.hfk-bremen.de/">faq.hfk-bremen.de</a> zusammengestellt. Bitte lese dort nach.<br/><br/>
+                Jeweils aktuelle und verbindliche Informationen zu den Corona-Maßnahmen und
+                Hygieneregeln findest du in deinem HfK-Postfach und unter{" "}
+                <a href="https://www.hfk-bremen.de/corona-info" target="_blank">
+                    hfk-bremen.de/corona-info
+                </a>.<br/>Bitte bleibe Informiert!
             </Text>
         </>
     );
