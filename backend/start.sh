f91cf24cf073a4d3d871f84f591dcaa8b58f6812
Leo <130.0x82@gmail.com>
2021-02-17 13:52:48 +0100

    backend: startscript for server

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,2 @@
-pipenv shell
-export DJANGO_SETTINGS_MODULE=checkin.settings.local
-export SITE_DOMAIN=localhost:8000
-python manage.py runserver
+postgres -D ~/postgres&
+pipenv run python manage.py runserver
