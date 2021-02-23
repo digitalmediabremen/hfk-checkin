d7479be24388c7f4089a74c740738afa157d72a9
jelko arnds <moin@jelkoarnds.com>
2021-02-23 19:02:34 +0100

    notifications: from resources v1 commit move NotificationType to type.py

--- backend/checkin/notifications/models.py
+++ backend/checkin/notifications/models.py
@@ -137,29 +137,15 @@ from post_office.models import EmailTemplate
 from .template_field import TemplateField
 from django.template.loader import get_template
 
-class NotificationTypes(models.TextChoices):
-    RESERVATION_REQUESTED = 'reservation_requested', _('Reservation requested')
-    RESERVATION_REQUESTED_OFFICIAL = 'reservation_requested_official', _('Reservation requested official')
-    RESERVATION_CANCELLED = 'reservation_cancelled', _('Reservation cancelled')
-    RESERVATION_CONFIRMED = 'reservation_confirmed', _('Reservation confirmed')
-    RESERVATION_DENIED = 'reservation_denied', _('Reservation denied')
-    RESERVATION_CREATED = 'reservation_created', _('Reservation created')
-    # If the access code is known at reservation time, this notification
-    # type is used.
-    RESERVATION_CREATED_WITH_ACCESS_CODE = 'reservation_created_with_access_code', _('Reservation created with access code')
-    # In some cases, the access code is known only some time after the
-    # reservation is made. A separate notification type is used so that
-    # we don't confuse the user with "new reservation created"-style
-    # messaging.
-    RESERVATION_ACCESS_CODE_CREATED = 'reservation_access_code_created', _('Reservation reservation_access_code_created')
-    RESERVATION_WAITING_FOR_PAYMENT = 'reservation_waiting_for_payment', _('Reservation reservation_waiting_for_payment')
-    RESERVATION_COMMENT_CREATED = 'reservation_comment_created', _('Reservation comment created')
+
+from .types import NotificationType
+
 
 class NotificationEmailTemplate(EmailTemplate):
     """ extends django-postoffice's EmailTemplate to include with file based templates and type definitions"""
 
     type = models.CharField(
-        verbose_name=_('Type'), choices=NotificationTypes.choices, max_length=100, unique=True, db_index=True, blank=True
+        verbose_name=_('Type'), choices=NotificationType.choices, max_length=100, unique=True, db_index=True, blank=True
     )
     #template = TemplateField(match='^notifications/.+\.html$', blank=True)
 
