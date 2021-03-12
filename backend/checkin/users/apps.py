fb5aaea971e7c0c95cc6627bf73d8fd6a3be08f8
jelko arnds <moin@jelkoarnds.com>
2021-03-12 17:24:43 +0100

    users: damn.

--- backend/checkin/users/apps.py
+++ backend/checkin/users/apps.py
@@ -47,8 +47,10 @@ def fix_microsoft_auth_user_admin():
 
         logger.info("Removing microsoft_auth hijacked Admin views.")
 
-        admin.site.unregister(MicrosoftAccount)
+        if admin.site.is_registered(MicrosoftAccount):
+            admin.site.unregister(MicrosoftAccount)
         # register microsoft_account's hijacked UserAdmin
-        admin.site.unregister(User)
-        # put our own UserAdmin back in to place
-        admin.site.register(User, UserAdmin)
+        if admin.site.is_registered(User):
+            admin.site.unregister(User)
+            # put our own UserAdmin back in to place
+            admin.site.register(User, UserAdmin)
