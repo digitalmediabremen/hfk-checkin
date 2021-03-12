37ec50b589bf937bd4bf59a749c5861ce6429123
Leo <130.0x82@gmail.com>
2021-03-12 19:54:48 +0100

    frontend: update translation file

--- frontend/localization/translation.ts
+++ frontend/localization/translation.ts
@@ -1,154 +1,183 @@
 import { Translation } from ".";
 
 const translation: Translation = {
-    "en": {
-      "common": {
-        "nicht verifiziert": "Not verified",
-        "Du hast bisher noch keine Reservierung angefragt.": "NEW",
-        "Seite nicht gefunden": "Page not found",
-        "+{days} Tag": "NEW",
-        "jetzt": "now",
-        "{fieldName} darf nicht leer sein.": "NEW",
-        "Vorname": "NEW",
-        "Nachname": "NEW",
-        "Telefonnummer": "NEW",
-        "Die Telefonnummer wird auschliesslich im Falle einer  Infektionsnachverfolgung verwendet.": "NEW",
-        "Raum übernehmen": "NEW",
-        "Zeit übernehmen": "enter time",
-        "Neue Anfrage": "NEW",
-        "Hilfe": "Help",
-        "Datenschutzinformationen": "Data & Privacy",
-        "Protokoll": "Log",
-        "Noch keine Checkins vorhanden": "No checkins",
-        "Telefon ändern": "Change phone",
-        "Datum und Uhrzeit": "NEW",
-        "Zeit": "NEW",
-        "Raum": "NEW",
-        "optionale angaben": "NEW",
-        "Externe Personen": "NEW",
-        "Pers.": "NEW",
-        "Buchungsgrund": "NEW",
-        "Grund": "NEW",
-        "Nachricht": "NEW",
-        "Nach.": "NEW",
-        "Meine Telefonnummer ({phone}) darf für Rückfragen verwendet werden.": "NEW",
-        "Anfragen": "NEW",
-        "Heute": "NEW",
-        "Morgen": "NEW"
-      },
-      "reservation": {
-        "Buchungen": "NEW",
-        "Weitere Anfrage": "NEW",
-        "Deine Anfrage ist eingegangen.": "NEW",
-        "Anfrage stornieren": "NEW",
-        "weitere Anfrage": "NEW"
-      },
-      "request-attendees": {
-        "HfK externe Person(en) anmelden": "NEW",
-        "Delete": "NEW",
-        "Person hinzufügen": "NEW",
-        "Weitere Person hinzufügen": "NEW",
-        "HfK externe Personen müssen vorab angemeldet werden. Deine Anfrage wird an die Corona-Beauftragte geschickt und geprüft. Dieser Vorgang kann deine Raumanfrage verzögern.": "NEW",
-        "Bitte nenne den Grund des Aufenthaltes der o.g. Person/en.": "NEW",
-        "Aufenthaltsgrund angeben": "NEW"
-      },
-      "request-resource": {
-        "Gebäude auswählen": "NEW",
-        "Raum suchen": "NEW",
-        "Name oder Nummer eingeben...": "NEW",
-        "Keine Ergebnisse": "NEW",
-        "Aus Liste wählen": "NEW",
-        "Wenn du dies für einen Fehler hälst, solltest du im Kommentar der Buchung deine Situation schildern.": "NEW",
-        "Kommentar hinzufügen": "NEW",
-        "Ich möchte den Raum allein nutzen.": "NEW"
-      },
-      "request-message": {
-        "Bitte erläutere Deine Anfrage nach Bedarf. \\n\\nZum Beispiel kannst Du hier Angaben für die Werkstattleitung machen oder dem Raumteam wichtige Informationen übermitteln.": "NEW"
-      },
-      "request-purpose": {
-        "Bitte ergänze deine Anfrage mit folgender Information.": "NEW",
-        "Begründung": "NEW",
-        "Normale Buchung": "NEW",
-        "Prüfung bzw. Prüfungsvorbereitung": "NEW",
-        "Abholung": "NEW",
-        "Gremiensitzung": "NEW",
-        "Anderer Grund": "NEW",
-        "Extern": "NEW",
-        "weitere": "NEW"
-      },
-      "request-time": {
-        "Bitte wähle ein frühreres Datum aus oder gib einen Buchungsgrund an.": "NEW",
-        "Buchungsgrund angeben": "NEW",
-        "Bitte rechne mit einer Bearbeitungszeit von mind. 48 Stunden.": "NEW",
-        "Wichtig: Am Wochenende werden in der Regel keine Anfragen bearbeitet. Willst du also eine Werkstatt für Montag um 10 Uhr buchen, stelle deine Anfrage bis spätestens Donnerstag 10 Uhr.": "NEW",
-        "Der Raum \\{resource}\\ kann maximal {days} Tage im Voraus gebucht werden.": "NEW",
-        "Räume können maximal {days} Tage im Vorraus gebucht werden.": "NEW"
-      },
-      "request": {
-        "Datum und Uhrzeit": "NEW",
-        "Raum": "NEW",
-        "Externe Personen": "NEW",
-        "Person hinzufügen": "NEW",
-        "Buchungsgrund": "NEW",
-        "Nachricht": "NEW",
-        "Raumliste": "NEW",
-        "Telefonkontakt": "NEW",
-        "Alleinnutzung": "NEW",
-        "Externe Teilnehmer": "NEW",
-        "Anfang": "NEW",
-        "Ende": "NEW",
-        "Weitere Teilnehmer": "NEW",
-        "Grund": "NEW",
-        "Du hast keine Berechtigung den Raum \\{resource}\\ zu buchen.": "NEW",
-        "Ein Ausnahmegrund muss angegeben werden.": "NEW",
-        "Du musst noch den Raum auswählen.": "NEW",
-        "Du musst noch eine Zeit angeben.": "NEW"
-      },
-      "createProfile": {
-        "Halte dich an die Regeln...": "In-person instruction and more flexible access to the HfK’s rooms will only be possible if everyone at the HfK observes all of the COVID-19 rules",
-        "Dokumentiere deinen Aufenthalt...": "Please be responsible about documenting the time you spend in the buildings and rooms of the HfK, and please follow the HfK hygiene rules",
-        "HfK-Angehörige": "HfK Members",
-        "Gäste": "Guests",
-        "Die Verifizierung mit Identitätsnachweis ist nach der Registrierung notwendig": "A photo ID verification at the front counter after registration is mandatory",
-        "Gastzugang": "Guest Access"
-      },
-      "setprofile": {
-        "erforderlich": "required",
-        "Vorname": "First name",
-        "Nachname": "Last name",
-        "Telefonnummer": "Phone number",
-        "Deine Angaben werden ausschließlich zur Rückverfolgung...": "Your data will only be used in case of illness. By registering, you confirm that you have read and understood HfK’s data protection policy; you also give permission to collect your data for the purpose of contact tracing, in case of illness; you have also read and understood the current hygiene rules and agree to observe them.",
-        "Registrieren": "Register",
-        "Speichern": "Save"
-      },
-      "verifyNow": {
-        "Identitätsprüfung": "Identity check",
-        "Bitte zeige diese Angaben dem Personal am Empfang.": "Please show this info at the front desk.",
-        "Sobald die Angaben durch das Personal geprüft und ihrerseits gespeichert wurden, kann der Prozess abgeschlossen werden.": "As soon as your ID is checked at the front desk your verification is done.",
-        "Abschliessen": "Close"
-      },
-      "verifyProfile": {
-        "Identitätsprüfung": "Identity check",
-        "Die Hochschule ist verpflichtet...": "HfK is obliged to verify identities of all people entering Speicher XI to ensure tracking in case of illness.",
-        "Bitte zeigen Sie jetzt einen Identitätsnachweis (Lichtbildausweis) am Empfang vor": "Please verify now with your photo ID at the front counter",
-        "Bevor ihre Identität nicht verifiziert ist, ist die Protokollierung und ein Eintritt nicht möglich.": "Entering Speicher XI is not possible unless verified.",
-        "Erledigt": "Done"
-      },
-      "checkin": {
-        "Erfolgreich ausgecheckt": "Checkout successful",
-        "Personen aktuell eingecheckt": "checked in overall",
-        "Auschecken": "Check out",
-        "Du bist noch an folgenden Orten eingecheckt:": "You're checked into following locations:",
-        "Du bist bereits eingecheckt": "You are already checked in",
-        "Checkin erfolgreich": "Checkin successful"
-      },
-      "enterCode": {
-        "Checkin / Checkout": "Checkin / Checkout",
-        "Bitte gib die 4-stellige Nummer des Standortes ein, den du jetzt betrittst oder verlässt.": "Please enter the four digit code at the location you want to exit or enter.",
-        "oder nutze den QR-Codes des Standorts um deinen Aufenthalt zu dokumentieren.": "Or scan the QR-Code at your location to check in.",
-        "Einchecken": "Check in"
-      },
-    }
-  }
+    en: {
+        common: {
+            "nicht verifiziert": "Not verified",
+            "Du hast bisher noch keine Reservierung angefragt.": "NEW",
+            "Seite nicht gefunden": "Page not found",
+            "+{days} Tag": "NEW",
+            jetzt: "now",
+            "{fieldName} darf nicht leer sein.": "NEW",
+            Vorname: "NEW",
+            Nachname: "NEW",
+            Telefonnummer: "NEW",
+            "Die Telefonnummer wird auschliesslich im Falle einer  Infektionsnachverfolgung verwendet.":
+                "NEW",
+            "Raum übernehmen": "NEW",
+            "Zeit übernehmen": "enter time",
+            "Neue Anfrage": "NEW",
+            Hilfe: "Help",
+            Datenschutzinformationen: "Data & Privacy",
+            Protokoll: "Log",
+            "Noch keine Checkins vorhanden": "No checkins",
+            "Telefon ändern": "Change phone",
+            "Datum und Uhrzeit": "NEW",
+            Zeit: "NEW",
+            Raum: "NEW",
+            "optionale angaben": "NEW",
+            "Externe Personen": "NEW",
+            "Pers.": "NEW",
+            Buchungsgrund: "NEW",
+            Grund: "NEW",
+            Nachricht: "NEW",
+            "Nach.": "NEW",
+            "Meine Telefonnummer ({phone}) darf für Rückfragen verwendet werden.":
+                "NEW",
+            Anfragen: "NEW",
+            Heute: "NEW",
+            Morgen: "NEW",
+        },
+        reservation: {
+            Buchungen: "NEW",
+            "Weitere Anfrage": "NEW",
+            "Deine Anfrage ist eingegangen.": "NEW",
+            "weitere Anfrage": "NEW",
+            "'Deine Buchung {identifier} wurde storniert.'": "NEW",
+            Stornieren: "NEW",
+            angefragt: "NEW",
+            bestätigt: "NEW",
+            abgelehnt: "NEW",
+            storniert: "NEW",
+            erstellt: "NEW",
+        },
+        request: {
+            "Externe Personen": "NEW",
+            Buchungsgrund: "NEW",
+            Nachricht: "NEW",
+            "Datum und Uhrzeit": "NEW",
+            Raum: "NEW",
+            "Person hinzufügen": "NEW",
+            Raumliste: "NEW",
+            Telefonkontakt: "NEW",
+            Alleinnutzung: "NEW",
+            "Externe Teilnehmer": "NEW",
+            Anfang: "NEW",
+            Ende: "NEW",
+            "Weitere Teilnehmer": "NEW",
+            Grund: "NEW",
+            "Du hast keine Berechtigung den Raum \\{resource}\\ zu buchen.":
+                "NEW",
+            "Ein Ausnahmegrund muss angegeben werden.": "NEW",
+            "Du musst noch den Raum auswählen.": "NEW",
+            "Du musst noch eine Zeit angeben.": "NEW",
+        },
+        "request-attendees": {
+            "HfK externe Person(en) anmelden": "NEW",
+            Delete: "NEW",
+            "Person hinzufügen": "NEW",
+            "Weitere Person hinzufügen": "NEW",
+            "HfK externe Personen müssen vorab angemeldet werden. Deine Anfrage wird an die Corona-Beauftragte geschickt und geprüft. Dieser Vorgang kann deine Raumanfrage verzögern.":
+                "NEW",
+            "Bitte nenne den Grund des Aufenthaltes der o.g. Person/en.": "NEW",
+            "Aufenthaltsgrund angeben": "NEW",
+        },
+        "request-resource": {
+            "Gebäude auswählen": "NEW",
+            "Raum suchen": "NEW",
+            "Name oder Nummer eingeben...": "NEW",
+            "Keine Ergebnisse": "NEW",
+            "Aus Liste wählen": "NEW",
+            "Wenn du dies für einen Fehler hälst, solltest du im Kommentar der Buchung deine Situation schildern.":
+                "NEW",
+            "Kommentar hinzufügen": "NEW",
+            "Ich möchte den Raum allein nutzen.": "NEW",
+        },
+        "request-message": {
+            "Bitte erläutere Deine Anfrage nach Bedarf. \\n\\nZum Beispiel kannst Du hier Angaben für die Werkstattleitung machen oder dem Raumteam wichtige Informationen übermitteln.":
+                "NEW",
+        },
+        "request-purpose": {
+            "Bitte ergänze deine Anfrage mit folgender Information.": "NEW",
+            Begründung: "NEW",
+            "Normale Buchung": "NEW",
+            "Prüfung bzw. Prüfungsvorbereitung": "NEW",
+            Abholung: "NEW",
+            Gremiensitzung: "NEW",
+            "Anderer Grund": "NEW",
+            Extern: "NEW",
+            weitere: "NEW",
+        },
+        "request-time": {
+            "Bitte wähle ein frühreres Datum aus oder gib einen Buchungsgrund an.":
+                "NEW",
+            "Buchungsgrund angeben": "NEW",
+            "Bitte rechne mit einer Bearbeitungszeit von mind. 48 Stunden.":
+                "NEW",
+            "Wichtig: Am Wochenende werden in der Regel keine Anfragen bearbeitet. Willst du also eine Werkstatt für Montag um 10 Uhr buchen, stelle deine Anfrage bis spätestens Donnerstag 10 Uhr.":
+                "NEW",
+            "Der Raum \\{resource}\\ kann maximal {days} Tage im Voraus gebucht werden.":
+                "NEW",
+            "Räume können maximal {days} Tage im Vorraus gebucht werden.":
+                "NEW",
+        },
+        createProfile: {
+            "Halte dich an die Regeln...":
+                "In-person instruction and more flexible access to the HfK’s rooms will only be possible if everyone at the HfK observes all of the COVID-19 rules",
+            "Dokumentiere deinen Aufenthalt...":
+                "Please be responsible about documenting the time you spend in the buildings and rooms of the HfK, and please follow the HfK hygiene rules",
+            "HfK-Angehörige": "HfK Members",
+            Gäste: "Guests",
+            "Die Verifizierung mit Identitätsnachweis ist nach der Registrierung notwendig":
+                "A photo ID verification at the front counter after registration is mandatory",
+            Gastzugang: "Guest Access",
+        },
+        setprofile: {
+            erforderlich: "required",
+            Vorname: "First name",
+            Nachname: "Last name",
+            Telefonnummer: "Phone number",
+            "Deine Angaben werden ausschließlich zur Rückverfolgung...":
+                "Your data will only be used in case of illness. By registering, you confirm that you have read and understood HfK’s data protection policy; you also give permission to collect your data for the purpose of contact tracing, in case of illness; you have also read and understood the current hygiene rules and agree to observe them.",
+            Registrieren: "Register",
+            Speichern: "Save",
+        },
+        verifyNow: {
+            Identitätsprüfung: "Identity check",
+            "Bitte zeige diese Angaben dem Personal am Empfang.":
+                "Please show this info at the front desk.",
+            "Sobald die Angaben durch das Personal geprüft und ihrerseits gespeichert wurden, kann der Prozess abgeschlossen werden.":
+                "As soon as your ID is checked at the front desk your verification is done.",
+            Abschliessen: "Close",
+        },
+        verifyProfile: {
+            Identitätsprüfung: "Identity check",
+            "Die Hochschule ist verpflichtet...":
+                "HfK is obliged to verify identities of all people entering Speicher XI to ensure tracking in case of illness.",
+            "Bitte zeigen Sie jetzt einen Identitätsnachweis (Lichtbildausweis) am Empfang vor":
+                "Please verify now with your photo ID at the front counter",
+            "Bevor ihre Identität nicht verifiziert ist, ist die Protokollierung und ein Eintritt nicht möglich.":
+                "Entering Speicher XI is not possible unless verified.",
+            Erledigt: "Done",
+        },
+        checkin: {
+            "Erfolgreich ausgecheckt": "Checkout successful",
+            "Personen aktuell eingecheckt": "checked in overall",
+            Auschecken: "Check out",
+            "Du bist noch an folgenden Orten eingecheckt:":
+                "You're checked into following locations:",
+            "Du bist bereits eingecheckt": "You are already checked in",
+            "Checkin erfolgreich": "Checkin successful",
+        },
+        enterCode: {
+            "Checkin / Checkout": "Checkin / Checkout",
+            "Bitte gib die 4-stellige Nummer des Standortes ein, den du jetzt betrittst oder verlässt.":
+                "Please enter the four digit code at the location you want to exit or enter.",
+            "oder nutze den QR-Codes des Standorts um deinen Aufenthalt zu dokumentieren.":
+                "Or scan the QR-Code at your location to check in.",
+            Einchecken: "Check in",
+        },
+    },
+};
 
 export default translation;
