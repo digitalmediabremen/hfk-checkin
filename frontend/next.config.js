e53eb4cca2487dce6fce46adc5fb69c1a66fa925
Leo <130.0x82@gmail.com>
2021-03-22 19:38:11 +0100

    frontend: fix runtime caching

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require("next-pwa");
 const features = require("./features");
 
+const runtimeCaching = require('next-pwa/cache')
+runtimeCaching[0].handler = 'StaleWhileRevalidate'
+
 module.exports = withPWA({
     pwa: {
         dest: "public",
