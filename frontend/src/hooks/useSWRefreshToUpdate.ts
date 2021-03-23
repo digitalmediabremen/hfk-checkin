003d690dac0926bc982f774031caca4577de3f39
Leo <130.0x82@gmail.com>
2021-03-23 15:29:45 +0100

    frontend: not tested: update service worker on page activation

--- frontend/src/hooks/useSWRefreshToUpdate.ts
+++ frontend/src/hooks/useSWRefreshToUpdate.ts
@@ -1,38 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { usePageVisibility } from "react-page-visibility";
 import { Workbox } from "workbox-window";
-import { WorkboxLifecycleWaitingEvent } from "workbox-window/utils/WorkboxEvent";
+import {
+    WorkboxLifecycleEvent,
+    WorkboxLifecycleWaitingEvent,
+} from "workbox-window/utils/WorkboxEvent";
 import { isClient } from "../../config";
 
 export default function useSWRefreshToUpdate() {
+    const visible = usePageVisibility();
+    const serviceWorkerRegistration = useRef<ServiceWorkerRegistration>();
+
+    useEffect(() => {
+        if (visible) serviceWorkerRegistration.current?.update();
+    }, [visible, serviceWorkerRegistration]);
+
     useEffect(() => {
         if (
-            isClient &&
-            "serviceWorker" in navigator &&
-            (window as any).workbox !== undefined
-        ) {
-            console.log("call it a hook");
-            const wb = new Workbox("/sw.js");
-            wb.addEventListener("installed", (event) => {
-                if (event.isUpdate) {
-                    console.log("reloaded due to sw update")
-                    window.location.reload(true);
-                }
-            });
-            // const promptNewVersionAvailable = (
-            //     event: WorkboxLifecycleWaitingEvent
-            // ) => {
-            //     console.log("send skip waiting");
-            //     wb.addEventListener("controlling", (event) => {
-            //         console.log("reload caused by service worker update");
-            //         window.location.reload();
-            //     });
+            !(
+                isClient &&
+                "serviceWorker" in navigator &&
+                (window as any).workbox !== undefined
+            )
+        )
+            return;
 
-            //     // Send a message to the waiting service worker, instructing it to activate.
-            //     wb.messageSkipWaiting();
-            // };
+        const wb = new Workbox("/sw.js");
+        const handleInstalled = (event: WorkboxLifecycleEvent) => {
+            if (event.isUpdate) {
+                console.log("reloaded due to sw update");
+                window.location.reload(true);
+            }
+        };
+        wb.addEventListener("installed", handleInstalled);
 
-            // wb.addEventListener("waiting", promptNewVersionAvailable);
-            wb.register();
-        }
+        wb.register().then((reg) => {
+            serviceWorkerRegistration.current = reg;
+        });
+
+        return () => {
+            wb.removeEventListener("installed", handleInstalled);
+        };
     }, []);
 }
