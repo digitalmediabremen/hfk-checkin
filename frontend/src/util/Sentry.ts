5c29a668c2547f9463f3ffafd8d3af300b7fc9c4
Leo <130.0x82@gmail.com>
2021-04-30 16:33:45 +0200

    frontend: specify sentry environment

--- frontend/src/util/Sentry.ts
+++ frontend/src/util/Sentry.ts
@@ -31,6 +31,7 @@ export const init = () => {
       integrations,
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       release: process.env.NEXT_PUBLIC_COMMIT_SHA,
+      environment: process.env.NEXT_PUBLIC_ENVIRONMENT || "production"
     })
   }
 }
