d3d9ac1e6d9cbdc37b2e4752628d52fd5810a08d
jelko arnds <moin@jelkoarnds.com>
2022-01-24 17:52:44 +0100

    backend: debug migration 029

--- backend/checkin/resources/management/commands/resources_convert_to_direct_object_permissions.py
+++ backend/checkin/resources/management/commands/resources_convert_to_direct_object_permissions.py
@@ -62,6 +62,8 @@ def convert_type(model, generic_model, direct_model, transfer_fields, out_method
 
     for instance in qs:
         dp_dict = generate_direct_permission_dict(instance, transfer_fields, using)
+        out_method("DEBUG: ")
+        out_method(dp_dict)
         direct_model.objects.create(**dp_dict)
         created_count += 1
         instance.delete()
